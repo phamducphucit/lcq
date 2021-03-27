d4b18885082bc6fb1dc0760189185d9d5c67290a
Kiet Nguyen <kantekcom@gmail.com>
2021-03-27 13:49:27 +0700

    David edit route file

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -15,6 +15,7 @@ class OrdersController extends Controller
     public function index()
     {
         //
+
     }
 
     /**
@@ -25,6 +26,7 @@ class OrdersController extends Controller
     public function create()
     {
         //
+        
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\OrdersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,43 +26,57 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['middleware'=>'auth'], function (){
+	// Users
+	Route::group(['prefix'=>'user'], function(){
+		Route::get('/list-users', [UsersController::class, 'index'])->name('list.users');
+		Route::get('/add-user', [UsersController::class, 'create'])->name('add.user');
+		Route::post('/add-user', [UsersController::class, 'store'])->name('add.user.post');
+		Route::get('/edit-user/{id}', [UsersController::class, 'edit'])->name('edit.user');
+		Route::post('/edit-user/{id}', [UsersController::class, 'update'])->name('update.user.post');
+		Route::get('/delete-user/{id}', [UsersController::class, 'destroy'])->name('delete.user');
+	});
+	// Role
+	Route::group(['prefix'=>'role'], function(){
+		Route::get('/list-roles', [RoleController::class, 'index'])->name('list.roles');
+		Route::get('/add-role', [RoleController::class, 'create'])->name('add.role');
+		Route::post('/add-role', [RoleController::class, 'store'])->name('add.role.post');
+		Route::get('/edit-role/{id}', [RoleController::class, 'edit'])->name('edit.role');
+		Route::post('/edit-role/{id}', [RoleController::class, 'update'])->name('update.role.post');
+		Route::get('/delete-role/{id}', [RoleController::class, 'destroy'])->name('delete.role');
+	});
 
-// Users
-Route::get('/list-users', [UsersController::class, 'index'])->name('list.users');
-Route::get('/add-user', [UsersController::class, 'create'])->name('add.user');
-Route::post('/add-user', [UsersController::class, 'store'])->name('add.user.post');
-Route::get('/edit-user/{id}', [UsersController::class, 'edit'])->name('edit.user');
-Route::post('/edit-user/{id}', [UsersController::class, 'update'])->name('update.user.post');
-Route::get('/delete-user/{id}', [UsersController::class, 'destroy'])->name('delete.user');
+	// Permission
+	Route::group(['prefix'=>'permission'], function(){
+		Route::get('/list-permissions', [PermissionController::class, 'index'])->name('list.permissions');
+		Route::get('/add-permission', [PermissionController::class, 'create'])->name('add.permission');
+		Route::post('/add-permission', [PermissionController::class, 'store'])->name('add.permission.post');
+		Route::get('/edit-permission/{id}', [PermissionController::class, 'edit'])->name('edit.permission');
+		Route::post('/edit-permission/{id}', [PermissionController::class, 'update'])->name('update.permission.post');
+		Route::get('/delete-permission/{id}', [PermissionController::class, 'destroy'])->name('delete.permission');
+	});
 
-// Role
-Route::get('/list-roles', [RoleController::class, 'index'])->name('list.roles');
-Route::get('/add-role', [RoleController::class, 'create'])->name('add.role');
-Route::post('/add-role', [RoleController::class, 'store'])->name('add.role.post');
-Route::get('/edit-role/{id}', [RoleController::class, 'edit'])->name('edit.role');
-Route::post('/edit-role/{id}', [RoleController::class, 'update'])->name('update.role.post');
-Route::get('/delete-role/{id}', [RoleController::class, 'destroy'])->name('delete.role');
+	// Product
+	Route::group(['prefix'=>'product'], function(){
+		Route::get('/list-products', [ProductController::class, 'index'])->name('list.products');
+		Route::get('/add-product', [ProductController::class, 'create'])->name('add.product');
+		Route::post('/add-product', [ProductController::class, 'store'])->name('add.product.post');
+		Route::get('/edit-product/{id}', [ProductController::class, 'edit'])->name('edit.product');
+		Route::post('/edit-product/{id}', [ProductController::class, 'update'])->name('update.product.post');
+		Route::get('/delete-product/{id}', [ProductController::class, 'destroy'])->name('delete.product');
+	});
 
+	// Orders
+	Route::group(['prefix'=>'order'], function(){
+		Route::get('/list-orders', [OrdersController::class, 'index'])->name('list.orders');
+		Route::get('/add-order', [OrdersController::class, 'create'])->name('add.order');
+		Route::post('/add-order', [OrdersController::class, 'store'])->name('add.order.post');
+		Route::get('/edit-order/{id}', [OrdersController::class, 'edit'])->name('edit.order');
+		Route::post('/edit-order/{id}', [OrdersController::class, 'update'])->name('update.order.post');
+		Route::get('/delete-order/{id}', [OrdersController::class, 'destroy'])->name('delete.order');
+	});
 
-// Permission
-Route::get('/list-permissions', [PermissionController::class, 'index'])->name('list.permissions');
-Route::get('/add-permission', [PermissionController::class, 'create'])->name('add.permission');
-Route::post('/add-permission', [PermissionController::class, 'store'])->name('add.permission.post');
-Route::get('/edit-permission/{id}', [PermissionController::class, 'edit'])->name('edit.permission');
-Route::post('/edit-permission/{id}', [PermissionController::class, 'update'])->name('update.permission.post');
-Route::get('/delete-permission/{id}', [PermissionController::class, 'destroy'])->name('delete.permission');
-
-
-// Product
-Route::get('/list-products', [ProductController::class, 'index'])->name('list.products');
-Route::get('/add-product', [ProductController::class, 'create'])->name('add.product');
-Route::post('/add-product', [ProductController::class, 'store'])->name('add.product.post');
-Route::get('/edit-product/{id}', [ProductController::class, 'edit'])->name('edit.product');
-Route::post('/edit-product/{id}', [ProductController::class, 'update'])->name('update.product.post');
-Route::get('/delete-product/{id}', [ProductController::class, 'destroy'])->name('delete.product');
-
-
-
+});
 
 // //Employee
 // Route::get('/employees', [EmployeesController::class, 'index'])->name('employees');
