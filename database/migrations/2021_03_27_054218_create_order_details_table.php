ae040d15539958bf8b9c8f08b246c023db302ecd
Kiet Nguyen <kantekcom@gmail.com>
2021-03-27 12:58:30 +0700

    Phuc Pham create table orders

---
+++ database/migrations/2021_03_27_054218_create_order_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->BigInteger('order_id');
+            $table->BigInteger('product_id');
+            $table->integer('quantity');
+            $table->double('price',100,2)->nullable(); 
+            $table->double('subtotal',100,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
