f81f98445a148ffc381bf22a73ad82b6fa575590
Kiet Nguyen <kantekcom@gmail.com>
2021-04-02 17:25:52 +0700

    david pussh code

--- resources/views/admin/orders/add_order.blade.php
+++ resources/views/admin/orders/add_order.blade.php
@@ -176,9 +176,7 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 <script type="text/javascript">
-    $(document).ready(function() {
-        $(".js-example-basic-single1").select2();
-    });
+    $(".js-example-basic-single1").select2();
 
     function getProducts1(){
         var id = document.getElementById("select_pro1").value;
